2782c1bcb9054e64de5d6893eb9eca3096b65a0e
emanameen9982773 <emanothard15@gmail.com>
2025-03-24 02:33:58 +0300

    method allocate is implemented

---
+++ Allocation Strategies/BestFit.java
@@ -0,0 +1,41 @@
+import java.util.LinkedList;
+
+public class BestFit extends AllocationStrategies {
+
+    public void allocate(int pID, int pSize, LinkedList<Block> blocks) {
+
+        if (isAllocated(pID, blocks) != -1)
+            System.out.printf("The Process P%d is already allocated in address %d", pID, isAllocated(pID, blocks));
+
+        else {
+
+            if (isFull(blocks))
+                System.out.println("Allocation Failed!! The memory is full, All Blocks are allocated\nYOU CAN DE-ALLOCATE SOME MEMORY BLOCKS");
+            else {
+
+                int smallestSize = Integer.MAX_VALUE;
+                for (Block block : blocks) {
+                    if (block.getSize() < smallestSize && block.getSize() <= pSize && block.getStatus().equals("free"))
+                        smallestSize = block.getSize();
+                }
+
+                if(smallestSize==Integer.MAX_VALUE){
+                    System.out.println("The Process is too big to fit in any free block in the memory :( ");
+                    return;
+                }
+
+                for (Block block : blocks) {
+                    if (block.getSize() == smallestSize && block.getStatus().equals("free")) {
+                        block.setStatus("allocated");
+                        block.setPID(pID);
+                        block.setInternalFragmentation(block.getSize() - pSize);
+                        break;
+                    }
+
+                }
+            }
+        }
+
+    }
+
+}
