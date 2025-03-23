ffff2ae3330e6d827002b5b6b56ec118767f3ce2
emanameen9982773 <emanothard15@gmail.com>
2025-03-24 02:34:58 +0300

    no impelmentation

---
+++ Allocation Strategies/FerstFit.java
@@ -0,0 +1,9 @@
+import java.util.LinkedList;
+
+public class FerstFit extends AllocationStrategies {
+
+    public void allocate(int pID, int pSize, LinkedList<Block> blocks  ){
+        
+    }
+}
+
---
+++ Allocation Strategies/WorstFit.java
@@ -0,0 +1,8 @@
+import java.util.LinkedList;
+
+public class WorstFit  extends AllocationStrategies {
+
+    public void allocate(int pID, int pSize , LinkedList<Block> blocks ){
+        
+    }
+}
