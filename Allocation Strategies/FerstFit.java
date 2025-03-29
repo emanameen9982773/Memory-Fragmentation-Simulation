ea1168c292b29c820d3ec8151728f2a5e028e9b4
LamaHosam6 <444200090@student.ksu.edu.sa>
2025-03-29 21:32:04 +0300

    Update FerstFit.java
    ..

--- Allocation Strategies/FerstFit.java
+++ Allocation Strategies/FerstFit.java
@@ -3,7 +3,27 @@ import java.util.LinkedList;
 public class FerstFit extends AllocationStrategies {
 
     public void allocate(int pID, int pSize, LinkedList<Block> blocks  ){
-        
+        if (isAllocated(pID, blocks) != -1) {
+            System.out.printf("The Process P%d is already allocated in address %d\n", pID, isAllocated(pID, blocks));
+            return;
+        }
+
+        if (isFull(blocks)) {
+            System.out.println("Allocation Failed!! The memory is full, All Blocks are allocated\nYOU CAN DE-ALLOCATE SOME MEMORY BLOCKS");
+            return;
+        }
+
+        for (Block block : blocks) {
+            if (block.getStatus().equals("free") && block.getSize() >= pSize) {
+                block.setStatus("allocated");
+                block.setPID(pID);
+                block.setInternalFragmentation(block.getSize() - pSize);
+                System.out.println("Process " + pID + " allocated at block starting at " + block.getStart());
+                return;  // توقف بعد العثور على أول كتلة مناسبة
+            }
+        }
+
+        System.out.println("The Process is too big to fit in any free block in the memory"); 
     }
 }
 
